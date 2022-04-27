53b7ecfd2a63c39711015facbcb680aecafdfbfb
Manish Bhattarai <hello@maneesh.com.np>
2022-04-27 07:16:52 +0545

    Website Added

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <title>Utshab Chapagain</title>
+  <link rel="shortcut icon" href="images/favicon.ico" />
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,700,800" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<div>
+  <div>
+    <div>
+      <div class="full-width-section">
+        <div class="section-wrapper">
+          <div>
+            <p class="site-des">Hello. My name is</p>
+            <h1 class="entry-title">Utshab Chapagain</h1>
+            <div class="social-links">
+              <a href="https://linkedin.com/in/#" target="blank">LINKEDIN</a>
+              <a href="https://twitter.com/#" target="blank">TWITTER</a>
+              <a href="https://medium.com/#" target="blank">MEDIUM</a>
+            </div>
+          </div>
+
+          </body>
+
+</html>
